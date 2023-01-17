5a9cddacd16b0407d2a54c6c0dee1d5fe7e167a5
Valdecir Luciano <valdecirlucianoti@gmail.com>
2023-01-17 10:41:33 -0300

    Configured DataBase H2 TESTE

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.devsuperior.dscatalog.entities.Category;
@@ -12,6 +11,7 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
+	
 	public List<Category> findAll() {
 		
 		return repository.findAll();
