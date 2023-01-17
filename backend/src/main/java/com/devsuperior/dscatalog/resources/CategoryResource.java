6fab2c4f5215d238ad29cf753424b37088d5b639
Valdecir Luciano <valdecirlucianoti@gmail.com>
2023-01-17 12:05:45 -0300

    Implementado o category DTO Data Transfer Object

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 
@@ -23,8 +24,8 @@ public class CategoryResource {
 	@GetMapping
 	//aqui eu programdo os endpoints
 	//configura que esta classe sera um weebservice, que será um endpoin do recurso category é necessario a anotação GETMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll(); 
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll(); 
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,8 +1,15 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -12,8 +19,22 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> categoryList = repository.findAll();
 		
-		return repository.findAll();
+		//Esta unica linha de código simplifica todo o for que ocorre nas linhas abaixo
+		return categoryList.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
+		
+		/*
+		List<CategoryDTO> listDTO = new ArrayList<>();
+		for (Category category : list) {
+			listDTO.add(new CategoryDTO(category));
+		}
+		return listDTO;
+		*/
+		
+		
+		 
 	}
 }
